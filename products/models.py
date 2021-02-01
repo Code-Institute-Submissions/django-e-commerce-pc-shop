dab31589b233c186cc8cfdcbeb7a936d4161f835
IvanTepes <ivan.tepes@outlook.com>
2021-02-01 21:45:16 +0000

    Started Setup app, added models to Setup, added fields to Product model

--- products/models.py
+++ products/models.py
@@ -15,12 +15,31 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
-    description = models.TextField()
+    sku = models.CharField(max_length=254, null=True, blank=False)
+    name = models.CharField(max_length=254, blank=False)
+    available = models.BooleanField(default=True, null=True, blank=False)
+    category = models.ForeignKey('Category', null=True, blank=False,
+                                 on_delete=models.SET_NULL)
+    brand = models.ForeignKey(
+        'setup.Brand', related_name="brands", null=True, blank=False, on_delete=models.SET_NULL)
+    key_feature = models.ForeignKey(
+        'setup.KeyFeatures', related_name="key_features", null=True, blank=False, on_delete=models.SET_NULL)
+    chipset_logo = models.ForeignKey(
+        'setup.Brand', related_name='chipset_logos',
+        null=True, blank=True, on_delete=models.SET_NULL)
+    additional_logo = models.ForeignKey(
+        'setup.Brand', related_name='additional_logos',
+        null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField(blank=False)
+    specification = models.ForeignKey(
+        'setup.Specification', related_name='Specs',
+        null=True, blank=False, on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+    promo_side_banner_left = models.ImageField(null=True, blank=True)
+    promo_side_banner_right = models.ImageField(null=True, blank=True)
+    promo_side_banner_bottom = models.ImageField(null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
