4b85af5737edfa711fc9abf244395b50915d812b
IvanTepes <ivan.tepes@outlook.com>
2021-02-01 11:37:43 +0000

    Added home app and templates

--- tech_cloud/settings.py
+++ tech_cloud/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,6 +44,9 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',  # social accounts
 
+    # My apps
+    'home'
+
 ]
 
 MIDDLEWARE = [
@@ -59,7 +64,10 @@ ROOT_URLCONF = 'tech_cloud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
