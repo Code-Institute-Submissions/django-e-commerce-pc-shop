0dacf7752c25aafbe3750754eae6bd360dffcf29
IvanTepes <ivan.tepes@outlook.com>
2021-02-02 18:48:10 +0000

    Added categories to home, load icons, position elements

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
