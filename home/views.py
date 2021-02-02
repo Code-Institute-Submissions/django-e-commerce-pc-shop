b7aed2016533c2899bfa5e0033a34d8649ac83d3
IvanTepes <ivan.tepes@outlook.com>
2021-02-02 14:31:58 +0000

    Added categories to index view

--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from products.models import Category
+
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
+    categories = Category.objects.all()
 
-    return render(request, 'home/index.html')
+    context = {
+        'categories': categories,
+    }
+    print(categories)
+    return render(request, 'home/index.html', context)
