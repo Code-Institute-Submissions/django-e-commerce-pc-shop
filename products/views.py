0a4b89e192d56b66c600e57ccd6b63ee6e42103d
IvanTepes <ivan.tepes@outlook.com>
2021-02-06 12:55:58 +0000

    Import setup categories in all product view, create function to return product detail

--- products/views.py
+++ products/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from setup.models import Category
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to return show all products, including sorting and search """
+    """ A view to return show all products, including sorting and search
+        And to return all categories names to use on dropdown menu
+     """
     products = Product.objects.all()
+    categories = Category.objects.all()
 
     context = {
         'products': products,
+        'categories': categories,
     }
 
     print(products)
+    print(categories)
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show product detail """
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    print(product)
+
+    return render(request, 'products/products_detail.html', context)
