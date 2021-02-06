aa736f3da00c06070c4c5101bde83ff957a37e52
IvanTepes <ivan.tepes@outlook.com>
2021-02-06 23:10:45 +0000

    Added all product viev foem product view to connect categories searching and filtering

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from products.models import Product
 from setup.models import Category
 
 # Create your views here.
@@ -14,3 +15,35 @@ def index(request):
     }
 
     return render(request, 'home/index.html', context)
+
+
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+    }
+
+    return render(request, 'products/products.html', context)
