73a88ba49b9c9838df3886a78366b050fb7e8718
IvanTepes <ivan.tepes@outlook.com>
2021-02-13 08:46:13 +0000

    Include brands urls

--- tech_cloud/urls.py
+++ tech_cloud/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
+    path('brands/', include('setup.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
