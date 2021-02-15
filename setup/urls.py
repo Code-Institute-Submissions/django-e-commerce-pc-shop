ac0f3780d90149b418c9623f85bf9799abce54d9
IvanTepes <ivan.tepes@outlook.com>
2021-02-15 15:20:30 +0000

    Include urls for keyfeaturs and features

--- setup/urls.py
+++ setup/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     path('brands/edit/<int:brand_id>/', views.edit_brand, name='edit_brand'),
     path('brands/delete/<int:brand_id>/', views.delete_brand, name='delete_brand'),
     path('keyfeatures/', views.all_keyfeatures, name='keyfeatures'),
+    path('keyfeatures/<int:keyfeatures_id>/', views.keyfeatures_detail, name='keyfeatures_detail'),
     path('keyfeatures/add/', views.add_keyfeatures, name='add_keyfeatures'),
+    path('keyfeatures/edit/<int:keyfeatures_id>/', views.edit_keyfeatures, name='edit_keyfeatures'),
+    path('keyfeatures/delete/<int:keyfeatures_id>/', views.delete_keyfeatures, name='delete_keyfeatures'),
+    path('features/', views.all_features, name='features'),
     path('feature/add', views.add_feature, name='add_feature'),
+    path('feature/delete/<int:feature_id>/', views.delete_feature, name='delete_feature'),
 ]
