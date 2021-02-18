004c8bae74a4b4db993e9ba541879094e4efda6d
IvanTepes <ivan.tepes@outlook.com>
2021-02-18 03:49:05 +0000

    Imstall dj_database and psycopg2 started deployment

--- tech_cloud/settings.py
+++ tech_cloud/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,11 +130,11 @@ WSGI_APPLICATION = 'tech_cloud.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
