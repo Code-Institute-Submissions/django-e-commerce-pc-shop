67e55bb0f53d982db78a65485b8a849acd338761
IvanTepes <ivan.tepes@outlook.com>
2021-02-18 05:06:58 +0000

    Deployment try

--- tech_cloud/settings.py
+++ tech_cloud/settings.py
@@ -141,6 +141,7 @@ else:
         }
     }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -201,7 +202,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
