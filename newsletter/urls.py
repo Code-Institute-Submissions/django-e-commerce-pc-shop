581ad808b03a7c3a4e118b8b21dbd5136e539fb0
IvanTepes <ivan.tepes@outlook.com>
2021-02-20 14:25:46 +0000

    Create url for unsubscribe from newsletter

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import newsletter_subscribe
+from .views import newsletter_subscribe, newsletter_unsubscribe
 
 app_name = 'newsletter'
 
 urlpatterns = [
     path('subscribe/', newsletter_subscribe, name='subscribe'),
+    path('unsubscribe/', newsletter_unsubscribe, name='unsubscribe'),
 ]
